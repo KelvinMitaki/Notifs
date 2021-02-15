c8355527c0ef4a57bbc2875da6f8ad10bbff0b9b
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-15 22:45:48 +0300

    added showing local notification

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-elements";
+import * as Notifications from "expo-notifications";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button
+        title="Trigger Notification"
+        onPress={() => {
+          Notifications.scheduleNotificationAsync({
+            content: {
+              title: "My first notification",
+              body: "This is the first local notification we are sending"
+            },
+            trigger: {
+              seconds: 10
+            }
+          });
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
